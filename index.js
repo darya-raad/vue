b5f2829c6aef15b726f2da72fc1b670d1149ba1c
Darya <darya.raadd@gmail.com>
2023-07-08 17:03:14 +0330

    added vue practice

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: "./image/vuepic.png",
+    url: "https://v2.vuejs.org",
+    imgAlt: "image",
+    title: "title",
+    onSale: true,
+  },
+});
