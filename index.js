fdaf2cbb279322269d87698c6a198a4be1b3b2ba
Darya <darya.raadd@gmail.com>
2023-07-28 14:32:45 +0330

    edit with issues

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "A pair of warm, fuzzy socks",
-    image: "./image/vuepic.png",
-    url: "https://v2.vuejs.org",
-    imgAlt: "image",
-    title: "title",
+    description: "“A pair of warm, fuzzy socks”",
+    imageAlt: "A pair of socks",
+    image: "./image/socks.jpg",
+    linkHref: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+    inStock: false,
     onSale: true,
   },
 });
