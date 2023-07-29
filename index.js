f0b3a589c16462e9d27bc53e36b26cf7e659da35
Darya <darya.raadd@gmail.com>
2023-07-29 18:30:10 +0330

    task 4

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    product: "Socks",
-    description: "“A pair of warm, fuzzy socks”",
-    imageAlt: "A pair of socks",
-    image: "./image/socks.jpg",
-    linkHref: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
-    inStock: false,
-    onSale: true,
-  },
-});
+
+  var app = new Vue({
+    el: "#app",
+    data: {
+      product: "Socks",
+      description: "“A pair of warm, fuzzy socks”",
+      imageAlt: "A pair of socks",
+      image: "./image/socksgreen.jpg",
+      linkHref:
+        "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+      inStock: false,
+      onSale: true,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./image/socksgreen.jpg",
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./image/socksblue.jpg",
+        },
+      ],
+      sizes: ["Small", "Medium", "Large"],
+    },
+    methods: {
+      updatePtoduct: function (variantImage) {
+        this.image = variantImage;
+      },
+    },
+  });
